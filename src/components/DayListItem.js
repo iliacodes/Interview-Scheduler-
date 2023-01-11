e27ddce05eba7039c63d7e84e11e465e7a948bef
iliacodes <6ac177ed@protonmail.com>
2023-01-11 11:06:53 -0800

    w7d2 exercises

---
+++ src/components/DayListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
+
+
+export default function DayListItem(props) {
+  const formatSpots = function (num) {
+    if (num === 0) {
+      return "no spots remaining"
+    } 
+    
+    else if (num === 1) {
+      return `${props.spots} spot remaining`
+    }
+
+    else {
+      return `${props.spots} spots remaining`
+    }
+  }
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+  console.log('dayClass', dayClass)
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
