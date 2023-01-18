fb80e1e2e9dd57c9457d2e5d80d6c41ac9a1aad0
iliacodes <6ac177ed@protonmail.com>
2023-01-18 02:40:39 -0800

    added and implemented prop-types into InterviewerList.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "components/InterviewerList.scss";
 import InterviewerListItem from 'components/InterviewerListItem';
+import PropTypes from 'prop-types'; 
 
 export default function InterviewerList(props) {
   const { interviewers, value, onChange } = props;
@@ -22,4 +23,8 @@ export default function InterviewerList(props) {
       <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   );
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 };
